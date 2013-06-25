c81d5e88030541b80e16672202cb4eb93585306a
Douglas Aguiar <douglasoa at gmail dot com>
2013-06-25 18:51:22 -0300

    Com problemas na edição dos posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController  
+  def update
+    @post = Post.find(params[:id])
+ 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
   def edit
-    
+    @post = Post.find(params[:id])
   end
   def index
     @posts = Post.all
