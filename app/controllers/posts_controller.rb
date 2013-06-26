4908ac00ab379aaa3133a4d6171b1f72ff6d9fd2
Douglas Aguiar <douglasoa@gmail.com>
2013-06-25 23:03:30 -0300

    CRUD completo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_path
+  end
   def update
     @post = Post.find(params[:id])
  
